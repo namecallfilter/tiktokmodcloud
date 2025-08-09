852ef658891b51f92d3688a28e522d8031a3db43
kaoru <namecallfilter@users.noreply.github.com>
2025-08-09 16:27:32 -0400

    feat: args

--- index.ts
+++ index.ts
@@ -2,6 +2,13 @@ import { createClient, getModDownload, getPluginDownload } from "./telegram";
 import { downloadFile } from "./utils";
 import { Api } from "telegram";
 
+const arg = process.argv[2];
+
+if (arg !== "mod" && arg !== "plugin") {
+	console.error("Usage: ts-node index.ts [mod|plugin]");
+	process.exit(1);
+}
+
 const client = await createClient();
 
 const channel = await client.getEntity("TikTokModCloud");
@@ -10,12 +17,12 @@ const messages = await client.getMessages(channel, {
 	limit: 10,
 });
 
-{
+if (arg === "mod") {
 	const [downloadLink, referer] = await getModDownload(messages);
 	await downloadFile(downloadLink, referer);
 }
 
-{
+if (arg === "plugin") {
 	const [downloadLink, referer] = await getPluginDownload(messages);
 	await downloadFile(downloadLink, referer);
 }
